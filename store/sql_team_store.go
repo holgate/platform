mattermost/mattermost

Adding ability to export data from mattermost

store/sql_team_store.go
@@ -203,3 +203,23 @@ func (s SqlTeamStore) GetTeamsForEmail(email string) StoreChannel {
 
 	return storeChannel
 }
+
+func (s SqlTeamStore) GetAll() StoreChannel {
+	storeChannel := make(StoreChannel)
+
+	go func() {
+		result := StoreResult{}
+
+		var data []*model.Team
+		if _, err := s.GetReplica().Select(&data, "SELECT * FROM Teams"); err != nil {
+			result.Err = model.NewAppError("SqlTeamStore.GetAll", "We could not get all teams", err.Error())
+		}
+
+		result.Data = data
+
+		storeChannel <- result
+		close(storeChannel)
+	}()
+
+	return storeChannel
+}

store/sql_user_store.go
@@ -5,9 +5,9 @@ package store
 
 import (
 	"fmt"
-	"strings"
 	"github.com/mattermost/platform/model"
 	"github.com/mattermost/platform/utils"
+	"strings"
 )
 
 type SqlUserStore struct {
@@ -168,7 +168,7 @@ func (us SqlUserStore) Update(user *model.User, allowActiveUpdate bool) StoreCha
 				nonUsernameKeys := []string{}
 				splitKeys := strings.Split(user.NotifyProps["mention_keys"], ",")
 				for _, key := range splitKeys {
-					if key != oldUser.Username && key != "@" + oldUser.Username {
+					if key != oldUser.Username && key != "@"+oldUser.Username {
 						nonUsernameKeys = append(nonUsernameKeys, key)
 					}
 				}
@@ -469,6 +469,33 @@ func (us SqlUserStore) VerifyEmail(userId string) StoreChannel {
 		}
 
 		result.Data = userId
+
+		storeChannel <- result
+		close(storeChannel)
+	}()
+
+	return storeChannel
+}
+
+func (us SqlUserStore) GetAll(teamId string) StoreChannel {
+
+	storeChannel := make(StoreChannel)
+
+	go func() {
+		result := StoreResult{}
+
+		var users []*model.User
+
+		if _, err := us.GetReplica().Select(&users, "SELECT * FROM Users WHERE TeamId = :TeamId", map[string]interface{}{"TeamId": teamId}); err != nil {
+			result.Err = model.NewAppError("SqlUserStore.GetProfiles", "We encounted an error while finding user profiles", err.Error())
+		} else {
+			for _, u := range users {
+				u.Password = ""
+				u.AuthData = ""
+			}
+
+			result.Data = users
+		}
 
 		storeChannel <- result
 		close(storeChannel)

holgate/platform

store/sql_team_store.go
@@ -193,3 +193,23 @@ func (s SqlTeamStore) GetTeamsForEmail(email string) StoreChannel {
 
 	return storeChannel
 }
+
+func (s SqlTeamStore) GetForExport() StoreChannel {
+	storeChannel := make(StoreChannel)
+
+	go func() {
+		result := StoreResult{}
+
+		var data []*model.Team
+		if _, err := s.GetReplica().Select(&data, "SELECT * FROM Teams"); err != nil {
+			result.Err = model.NewAppError("SqlTeamStore.GetForExport", "We could not get all teams", err.Error())
+		}
+
+		result.Data = data
+
+		storeChannel <- result
+		close(storeChannel)
+	}()
+
+	return storeChannel
+}

store/sql_user_store.go
@@ -452,3 +452,30 @@ func (us SqlUserStore) VerifyEmail(userId string) StoreChannel {
 
 	return storeChannel
 }
+
+func (us SqlUserStore) GetForExport(teamId string) StoreChannel {
+
+	storeChannel := make(StoreChannel)
+
+	go func() {
+		result := StoreResult{}
+
+		var users []*model.User
+
+		if _, err := us.GetReplica().Select(&users, "SELECT * FROM Users WHERE TeamId = :TeamId", map[string]interface{}{"TeamId": teamId}); err != nil {
+			result.Err = model.NewAppError("SqlUserStore.GetProfiles", "We encounted an error while finding user profiles", err.Error())
+		} else {
+			for _, u := range users {
+				u.Password = ""
+				u.AuthData = ""
+			}
+
+			result.Data = users
+		}
+
+		storeChannel <- result
+		close(storeChannel)
+	}()
+
+	return storeChannel
+}